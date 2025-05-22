96d9a4b0f8fbf4cc4b62b592fde60c7fe873f524
bg7758 <126126712+bg7758@users.noreply.github.com>
2025-05-22 18:27:39 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -34,10 +34,7 @@ const API_SITES = {
         api: 'https://bfzyapi.com/api.php/provide/vod',
         name: '暴风资源',
     },
-    tyyszy: {
-        api: 'https://tyyszy.com/api.php/provide/vod',
-        name: '天涯资源',
-    },
+    
     // xiaomaomi: {
     //     api: 'https://zy.xiaomaomi.cc/api.php/provide/vod',
     //     name: '小猫咪资源',
@@ -46,6 +43,10 @@ const API_SITES = {
         api: 'http://ffzy5.tv/api.php/provide/vod',
         name: '非凡影视',
         detail: 'http://ffzy5.tv', 
+    },
+    tyyszy: {
+        api: 'https://tyyszy.com/api.php/provide/vod',
+        name: '天涯资源',
     },
     heimuer: {
         api: 'https://json.heimuer.xyz/api.php/provide/vod',
